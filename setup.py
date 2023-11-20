431d8c37dcfd1f83107a7da537e30da3253523c1
Kang Zheng <zhengkang@meta.com>
2023-11-20 04:12:30 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="cubercnn",  # Name of your package
+    version="0.1",  # Package version
+    packages=["cubercnn"],  # Specify the package directory to include
+)
