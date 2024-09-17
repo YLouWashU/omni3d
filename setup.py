a5bcbf8fd45e9f69e7ec9de1cebea9e82226d6e2
Yang Lou <yang.lou@oculus.com>
2024-09-17 15:37:35 -0700

    Add automatic package finding.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="cubercnn",  # Name of your package
     version="0.1",  # Package version
-    packages=["cubercnn"],  # Specify the package directory to include
+    packages=find_packages(),  # Specify the package directory to include
 )
